3892be2b3b1afd7238f055ecadaa43eafa3506be
jack-a-dandy <jack-a-dandy@protonmail.ch>
2021-08-14 15:46:31 +0300

    Add children categories field for category form and relationship validation

--- django_shop_admin/core/admin.py
+++ django_shop_admin/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.admin.widgets import AdminFileWidget
+from django.contrib.admin.widgets import AdminFileWidget, FilteredSelectMultiple
 from django.utils.safestring import mark_safe
 from .models import Shop, Category, Product
 from django.db.models import ImageField, Q
@@ -8,6 +8,12 @@ from admin_numeric_filter.admin import RangeNumericFilter, NumericFilterModelAdm
 from django.utils.html import format_html
 from django.urls import path, reverse
 from django.template.response import TemplateResponse
+from django.db import transaction
+from django.contrib.admin.options import (
+	PermissionDenied, unquote, DisallowedModelAdminToField,
+	flatten_fieldsets, all_valid, IS_POPUP_VAR, TO_FIELD_VAR, 
+	helpers, _
+)
 
 # Register your models here.
 class AdminImageWidget(AdminFileWidget):
@@ -24,7 +30,8 @@ class AdminImageWidget(AdminFileWidget):
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-	list_display = ('title',)
+	list_display = ('title','id')
+	fields = ('id', 'title', 'description', 'imageUrl')
 	search_fields = ('title',)
 	ordering = ('title',)
 	readonly_fields = ('id',)
@@ -36,7 +43,7 @@ class ParentCategoryFilter(admin.SimpleListFilter):
 	parameter_name = 'parents__id'
 
 	def lookups(self, request, model_admin):
-		objs = Category.objects.all().only('title').order_by('title')
+		objs = Category.objects.filter(from_category__isnull=False).only('title').distinct().order_by('title')
 		return [(o.pk, o.title) for o in objs]
 
 	def queryset(self, request, queryset):
@@ -47,26 +54,71 @@ class ParentCategoryFilter(admin.SimpleListFilter):
 
 
 class CategoryAdminForm(forms.ModelForm):
+	parents = forms.ModelMultipleChoiceField(
+				queryset = Category.objects.only('title').order_by('title'),
+				required=False,
+				widget=FilteredSelectMultiple(
+						verbose_name='Родительские категории',
+						is_stacked=False
+					))
+
+	children = forms.ModelMultipleChoiceField(
+				queryset=Category.objects.only('title').order_by('title'),
+				required=False,
+				widget=FilteredSelectMultiple(
+						verbose_name='Дочерние категории',
+						is_stacked=False
+					)
+				)
+
 	class Meta:
 		model = Category
-		fields = ('id', 'title', 'description', 'parents')
+		fields = '__all__'
 
 	def __init__(self, *args, **kwargs):
 		super(CategoryAdminForm, self).__init__(*args, **kwargs)
 		instance = kwargs.get("instance")
-		if instance:
-			self.fields['parents'].queryset = Category.objects.filter(~(Q(pk=instance.pk)|Q(pk__in=instance.children.values('id'))))
+		if instance and instance.pk:
+			self.fields['parents'].queryset=Category.objects.filter(
+						~(Q(pk=instance.pk)|Q(pk__in=instance.category_set.values('id')))
+						).only('title').order_by('title')
+			self.fields['parents'].initial=instance.parents.all()
+			self.fields['children'].queryset=Category.objects.filter(
+							~(Q(pk=instance.pk)|Q(pk__in=instance.parents.values('id')))
+					).only('title').order_by('title')
+			self.fields['children'].initial=instance.category_set.all()
+
+	def save(self, commit=True):
+		category = super(CategoryAdminForm, self).save(commit=False)
+		if commit:
+			error=1
+			try:
+				with transaction.atomic():
+					category.save()
+					data = self.cleaned_data['parents']
+					if self.fields['parents'].has_changed(self.fields['parents'].initial, data):
+						category.parents.set(data)
+					error=2
+					data = self.cleaned_data['children']
+					if self.fields['children'].has_changed(self.fields['children'].initial, data):
+						category.category_set.set(data)
+			except forms.ValidationError as e:
+				self.add_error('parents' if error==1 else 'children', e)
+			except Exception as e:
+				self.add_error(None, e)
+		return category
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ('title','category_actions')
+	list_display = ('title','id','category_actions')
 	search_fields = ('products__id', 'title')
 	list_filter = (ParentCategoryFilter,)
 	ordering = ('title',)
 	readonly_fields = ('id',)
 	form = CategoryAdminForm
-	filter_horizontal = ('parents',)
+
+	change_form_template = 'admin/category_change_form.html'
 
 	def get_urls(self):
 		urls = super().get_urls()
@@ -101,13 +153,131 @@ class CategoryAdmin(admin.ModelAdmin):
 			context,
 		)
 
+	def save_form(self, request, form, change):
+		return form.save()
+
+	#Немного отредактированный метод класса ModelAdmin 
+	#для отображения ошибок при сохранении родительских/дочерних категорий
+	def _changeform_view(self, request, object_id, form_url, extra_context):
+		to_field = request.POST.get(TO_FIELD_VAR, request.GET.get(TO_FIELD_VAR))
+		if to_field and not self.to_field_allowed(request, to_field):
+			raise DisallowedModelAdminToField("The field %s cannot be referenced." % to_field)
+
+		model = self.model
+		opts = model._meta
+
+		if request.method == 'POST' and '_saveasnew' in request.POST:
+			object_id = None
+
+		add = object_id is None
+
+		if add:
+			if not self.has_add_permission(request):
+				raise PermissionDenied
+			obj = None
+
+		else:
+			obj = self.get_object(request, unquote(object_id), to_field)
+
+			if request.method == 'POST':
+				if not self.has_change_permission(request, obj):
+					raise PermissionDenied
+			else:
+				if not self.has_view_or_change_permission(request, obj):
+					raise PermissionDenied
+
+			if obj is None:
+				return self._get_obj_does_not_exist_redirect(request, opts, object_id)
+
+		fieldsets = self.get_fieldsets(request, obj)
+		ModelForm = self.get_form(
+			request, obj, change=not add, fields=flatten_fieldsets(fieldsets)
+		)
+		if request.method == 'POST':
+			form = ModelForm(request.POST, request.FILES, instance=obj)
+			form_validated = form.is_valid()
+			if form_validated:
+				new_object = self.save_form(request, form, change=not add)
+				form_validated = form.is_valid()
+			else:
+				new_object = form.instance
+			formsets, inline_instances = self._create_formsets(request, new_object, change=not add)
+			if all_valid(formsets) and form_validated:
+				change_message = self.construct_change_message(request, form, formsets, add)
+				if add:
+					self.log_addition(request, new_object, change_message)
+					return self.response_add(request, new_object)
+				else:
+					self.log_change(request, new_object, change_message)
+					return self.response_change(request, new_object)
+			else:
+				form_validated = False
+		else:
+			if add:
+				initial = self.get_changeform_initial_data(request)
+				form = ModelForm(initial=initial)
+				formsets, inline_instances = self._create_formsets(request, form.instance, change=False)
+			else:
+				form = ModelForm(instance=obj)
+				formsets, inline_instances = self._create_formsets(request, obj, change=True)
+
+		if not add and not self.has_change_permission(request, obj):
+			readonly_fields = flatten_fieldsets(fieldsets)
+		else:
+			readonly_fields = self.get_readonly_fields(request, obj)
+		adminForm = helpers.AdminForm(
+			form,
+			list(fieldsets),
+			# Clear prepopulated fields on a view-only form to avoid a crash.
+			self.get_prepopulated_fields(request, obj) if add or self.has_change_permission(request, obj) else {},
+			readonly_fields,
+			model_admin=self)
+		media = self.media + adminForm.media
+
+		inline_formsets = self.get_inline_formsets(request, formsets, inline_instances, obj)
+		for inline_formset in inline_formsets:
+			media = media + inline_formset.media
+
+		if add:
+			title = _('Add %s')
+		elif self.has_change_permission(request, obj):
+			title = _('Change %s')
+		else:
+			title = _('View %s')
+		context = {
+			**self.admin_site.each_context(request),
+			'title': title % opts.verbose_name,
+			'subtitle': str(obj) if obj else None,
+			'adminform': adminForm,
+			'object_id': object_id,
+			'original': obj,
+			'is_popup': IS_POPUP_VAR in request.POST or IS_POPUP_VAR in request.GET,
+			'to_field': to_field,
+			'media': media,
+			'inline_admin_formsets': inline_formsets,
+			'errors': helpers.AdminErrorList(form, formsets),
+			'preserved_filters': self.get_preserved_filters(request),
+		}
+
+		# Hide the "Save" and "Save and continue" buttons if "Save as New" was
+		# previously chosen to prevent the interface from getting confusing.
+		if request.method == 'POST' and not form_validated and "_saveasnew" in request.POST:
+			context['show_save'] = False
+			context['show_save_and_continue'] = False
+			# Use the change template instead of the add template.
+			add = False
+
+		context.update(extra_context or {})
+
+		return self.render_change_form(request, context, add=add, change=not add, obj=obj, form_url=form_url)
+
 
 class ShopFilter(admin.SimpleListFilter):
 	title = 'Магазин'
 	parameter_name = 'shop__id'
 
 	def lookups(self, request, model_admin):
-		objs = Shop.objects.all().only('title').order_by('title')
+		objs = Shop.objects.filter(products__isnull=False).only('title').distinct().order_by('title')
 		return [(o.pk, o.title) for o in objs]
 
 	def queryset(self, request, queryset):
@@ -122,7 +292,7 @@ class CategoryFilter(admin.SimpleListFilter):
 	parameter_name = 'categories__id'
 
 	def lookups(self, request, model_admin):
-		objs = Category.objects.all().only('title').order_by('title')
+		objs = Category.objects.filter(products__isnull=False).only('title').distinct().order_by('title')
 		return [(o.pk, o.title) for o in objs]
 
 	def queryset(self, request, queryset):
@@ -139,9 +309,15 @@ class MyNumericRangeFilter(RangeNumericFilter):
 @admin.register(Product)
 class ProductAdmin(NumericFilterModelAdmin):
 	list_display = ('title', 'id', 'amount', 'price', 'active', 'shop')
+	fields = ('id', 'active', 'title', 'description', 'amount', 'price', 'categories', 'shop')
 	search_fields = ('id', 'title')
 	list_filter = ('active',('price',MyNumericRangeFilter), 
 		ShopFilter, CategoryFilter)
 	readonly_fields = ('id',)
 	filter_horizontal = ('categories',)
+
+	def formfield_for_manytomany(self, db_field, request, **kwargs):
+		if db_field.name == "categories":
+			kwargs["queryset"] = Category.objects.only('title').order_by('title')
+		return super().formfield_for_manytomany(db_field, request, **kwargs)
 
--- django_shop_admin/core/models.py
+++ django_shop_admin/core/models.py
@@ -1,7 +1,10 @@
 from django.db.models import (Model, CharField, TextField, ImageField, 
 	BooleanField, PositiveIntegerField, DecimalField, ForeignKey, ManyToManyField,
-	CASCADE, CheckConstraint, Q)
+	CASCADE, CheckConstraint, UniqueConstraint, Q, F)
 from django.conf import settings
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 
@@ -26,8 +29,8 @@ class Shop(Model):
 class Category(Model):
 	title = CharField(verbose_name='Название', max_length=200, unique=True)
 	description = TextField(verbose_name='Описание', null=True, blank=True)
-	parents = ManyToManyField('self', symmetrical=False, related_name='children', 
-		blank=True, verbose_name='Род. категории')
+	parents = ManyToManyField('self', symmetrical=False, through='CategoryParent', 
+		blank=True, verbose_name='Родительские категории')
 
 	def __str__(self):
 		return self.title
@@ -50,6 +53,51 @@ class Category(Model):
 		constraints = (
 				CheckConstraint(check=Q(title__iregex=r'^\S.*\S$'), name='category_title_check'),
 		)
+
+
+class CategoryParent(Model):
+	from_category = ForeignKey(Category, on_delete=CASCADE, 
+		verbose_name='Дочерняя категория')
+	to_category = ForeignKey(Category, on_delete=CASCADE, 
+		related_name='from_category', verbose_name='Родительская категория')
+
+	class Meta:
+		constraints = (
+				UniqueConstraint(fields=('from_category', 'to_category'), name='unique_category_parent'),
+				CheckConstraint(check=~Q(from_category=F('to_category')), name='self_parent_category_check')
+			)
+		verbose_name = "Отношение категорий"
+		verbose_name_plural = "Отношения категорий"
+
+	@transaction.atomic
+	def save(self, *args, **kwargs):
+		check_child_in_parents(self.from_category_id, self.to_category_id, self.to_category_id)
+		super(CategoryParent, self).save(*args, **kwargs)
+
+
+def check_child_in_parents(from_id, to_id, start_to, checked=set()):
+	if CategoryParent.objects.filter(from_category_id=to_id, to_category_id=from_id).exists():
+			raise ValidationError(f'Доч. категория {Category.objects.get(id=from_id).title} не может быть родительской '\
+				f"для {Category.objects.filter(pk=start_to).values_list('title', flat=True)[0]} "\
+				f"({Category.objects.filter(pk=to_id).values_list('title', flat=True)[0]}).")
+	else:
+		for i in CategoryParent.objects.filter(from_category_id=to_id).values_list('to_category_id', flat=True):
+			if i not in checked:
+				checked.add(i)
+				check_child_in_parents(from_id, i, start_to, checked)
+
+
+def process_m2m_category_update(sender, instance, action, reverse, pk_set, **kwargs):
+	if action == 'pre_add':
+		if reverse:
+			for k in pk_set:
+				check_child_in_parents(k, instance.pk, instance.pk)
+		else:
+			for k in pk_set:
+				check_child_in_parents(instance.pk, k, k)
+	
+
+m2m_changed.connect(process_m2m_category_update, sender=CategoryParent)
 
 
 class Product(Model):
